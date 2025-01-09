6bb384c44e4ddb1483c1945750eb9d9772c5c64d
yszaryszar <yszaryszar@gmail.com>
2025-01-09 09:49:34 +0800

    build: :package: 优化了 Docker 镜像以减小体积和提高构建效率，移除了不必要的依赖，添加了自动导入插件以提升开发体验，并简化了 Nginx 配置以便更好地管理静态文件和后端服务

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,9 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import Inspector from 'vite-plugin-vue-inspector' // OR vite-plugin-vue-inspector
@@ -11,6 +14,12 @@ export default defineConfig({
   plugins: [
     vue(),
     Inspector(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
     // vueDevTools(),
     vueDevTools({
       launchEditor: 'webstorm',
